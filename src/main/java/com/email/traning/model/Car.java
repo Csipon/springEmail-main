c38d8c8a1fb7591085acbbb67a543588cde1a664
Pasha <oxeygenoxeygen@gmail.com>
2017-06-05 14:53:16 +0300

    feature - #1 - add model car

---
+++ src/main/java/com/email/traning/model/Car.java
@@ -0,0 +1,35 @@
+package com.email.traning.model;
+
+/**
+ * Created by Pasha on 05.06.2017.
+ */
+public class Car {
+    private String model;
+    private String mark;
+    private Integer year;
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
