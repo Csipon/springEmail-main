2b60ca4259f41ff74f5e0cf530a0486cd7bd1fdd
Pasha <oxeygenoxeygen@gmail.com>
2017-06-05 13:39:53 +0300

    Initial commit

---
+++ src/main/java/com/email/traning/Application.java
@@ -0,0 +1,15 @@
+package com.email.traning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Pasha on 05.06.2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
