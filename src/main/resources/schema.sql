48a49012582677c1e6455aa3660e5be70623a1a3
Smeet <Andryuha_91@list.ru>
2017-06-14 09:35:54 +0200

    fix - #14 - sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,16 +2,6 @@ DROP SCHEMA IF EXISTS public CASCADE;
 COMMIT;
 CREATE SCHEMA public;
 COMMIT;
--- DROP TABLE IF EXISTS orders CASCADE;
--- DROP TABLE IF EXISTS users CASCADE;
--- DROP TABLE IF EXISTS user_roles CASCADE;
--- DROP TABLE IF EXISTS car_details CASCADE;
---
--- DROP TABLE IF EXISTS users;
--- DROP TABLE IF EXISTS user_roles;
--- DROP TABLE IF EXISTS car_details;
--- DROP TABLE IF EXISTS cars;
--- DROP TABLE IF EXISTS orders;
 
 CREATE TABLE user_roles (
   id INTEGER not null,
@@ -37,6 +27,7 @@ CREATE TABLE cars (
   year INTEGER NOT NULL,
   price_per_hour Float NOT NULL,
   car_details_id INTEGER,
+  statuses_id INTEGER,
   CONSTRAINT cars_PK PRIMARY KEY (id)
 );
 
@@ -61,14 +52,47 @@ CREATE TABLE orders (
   due_date TIMESTAMP NOT NULL,
   total_price float NOT NULL,
   car_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  CONSTRAINT orders_pk PRIMARY KEY (id)
+  customer_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL,
+  status_id INTEGER NOT NULL,
+  CONSTRAINT orders_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE statuses (
+  id BIGSERIAL NOT NULL,
+  status varchar,
+  CONSTRAINT statuses_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE history (
+  id BIGSERIAL NOT NULL,
+  date TIMESTAMP NOT NULL,
+  description TEXT,
+  orders_id INTEGER NOT NULL,
+  change_statuses_id INTEGER NOT NULL
 );
 
 ALTER TABLE users ADD CONSTRAINT user_roles_id_FK FOREIGN KEY (user_roles_id) REFERENCES user_roles(id);
 ALTER TABLE orders ADD CONSTRAINT cars_id_FK FOREIGN KEY (car_id) REFERENCES cars(id);
-ALTER TABLE orders ADD CONSTRAINT users_id_FK FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE cars ADD CONSTRAINT car_details_id_FK FOREIGN KEY (car_details_id) REFERENCES car_details(id);
+ALTER TABLE orders ADD CONSTRAINT customer_id_FK FOREIGN KEY (customer_id) REFERENCES users(id);
+ALTER TABLE orders ADD CONSTRAINT manager_id_FK FOREIGN KEY (manager_id) REFERENCES users(id);
+ALTER TABLE orders ADD CONSTRAINT statuses_id_FK FOREIGN KEY (status_id) REFERENCES statuses(id);
+ALTER TABLE cars ADD CONSTRAINT cars_details_id_FK FOREIGN KEY (car_details_id) REFERENCES car_details(id);
+ALTER TABLE cars ADD CONSTRAINT cars_statuses_FK FOREIGN KEY (statuses_id) REFERENCES statuses(id);
+ALTER TABLE history ADD CONSTRAINT history_orders_FK FOREIGN KEY (orders_id) REFERENCES orders(id);
+ALTER TABLE history ADD CONSTRAINT history_statuses_FK FOREIGN KEY (change_statuses_id) REFERENCES statuses(id);
 
 INSERT INTO user_roles (id, role) VALUES (1, 'ROLE_USER');
 INSERT INTO user_roles (id, role) VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO statuses(status) VALUES ('ENABLE');
+INSERT INTO statuses(status) VALUES ('RESERVED');
+INSERT INTO statuses(status) VALUES ('ORDERED');
+
+INSERT INTO statuses(status) VALUES ('NEW');
+INSERT INTO statuses(status) VALUES ('PROCESSING');
+INSERT INTO statuses(status) VALUES ('ACTIVE');
+INSERT INTO statuses(status) VALUES ('FINISHED');
+
+
+
